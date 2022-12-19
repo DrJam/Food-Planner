d42d5a5370c5c85adb3ef3e876fc65794f4a0951
DrJam <dan@fordsville.co.uk>
2022-12-19 18:59:41 +0000

    add test db load

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+const { Datastore } = require('@google-cloud/datastore')
 const express = require('express');
 const app = express();
+
+const datastore = new Datastore()
+
+async function quickstart() {
+    const kind = 'Task'
+    const name = 'Task1'
+    const taskKey = datastore.key([kind, name])
+    let p = datastore.get(taskKey)
+    p.then(v => {
+        debugger
+        console.log(v.description)
+    })
+
+    // console.log(`saved `)
+}
+quickstart()
 
 app.get('/', (req, res) => {
     const name = process.env.NAME || 'World';
